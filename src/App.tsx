adecd15f5f0badf4039914c2976737d9591a646b
Wilson Caetano <wilsonjr_caetano@hotmail.com>
2020-10-12 15:09:52 -0300

    Separa tela incial em um novo arquivo

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,10 @@ import React from "react";
 import "./styles/pages/landing.css";
 import logo from "./images/logo.svg";
 import { FiArrowDownRight } from "react-icons/fi";
+import Routes from "./routes";
 
 function App() {
-  return (
-    <div id="page-landing">
-      <div className="content-wrapper">
-        <img src={logo} />
-        <main>
-          <h1>Leve felicidade para o mundo</h1>
-          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
-        </main>
-        <div className="location">
-          <strong>Americana</strong>
-          <span>São Paulo</span>
-        </div>
-        <a href="" className="enter-app">
-          <FiArrowDownRight size={26} color="rgba(0,0,0,0.6)" />
-        </a>
-      </div>
-    </div>
-  );
+  return <Routes />;
 }
 
 export default App;
