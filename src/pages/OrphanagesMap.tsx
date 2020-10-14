e4a5340ecce4618f373ce6b396b8d5273aba0171
Wilson Caetano <wilsonjr_caetano@hotmail.com>
2020-10-14 16:41:45 -0300

    Configura o marcador de mapa que indica locais onde há orfanatos

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import logo from "../images/map-maker.svg";
 import { Link } from "react-router-dom";
-import { FiPlus } from "react-icons/fi";
-import { Map, TileLayer } from "react-leaflet";
+import { FiPlus, FiArrowRight } from "react-icons/fi";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 import "../styles/pages/orphaneges.css";
 import "leaflet/dist/leaflet.css";
-import { access } from "fs";
+import Leaflet from "leaflet";
 
 function OrphanagesMap() {
+  const mapIcon = Leaflet.icon({
+    iconUrl: logo,
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+  });
   return (
     <div id="page-map">
       <aside>
@@ -29,6 +35,19 @@ function OrphanagesMap() {
         <TileLayer
           url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX}`}
         />
+        <Marker position={[-22.7238733, -47.3608195]} icon={mapIcon}>
+          <Popup
+            closeButton={false}
+            minWidth={240}
+            maxWidth={240}
+            className="map-popup"
+          >
+            Lar das meninas
+            <Link to="#">
+              <FiArrowRight size={20} color="#FFF" />
+            </Link>
+          </Popup>
+        </Marker>
       </Map>
 
       <Link to="" className="create-orphanage">
